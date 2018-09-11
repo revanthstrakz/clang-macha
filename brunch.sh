71ab47e72df4b490d13c8162ae8af54d02bdfdc7
Revanth Strakz <dr.revanthstrakz@gmail.com>
2018-09-11 16:49:51 +0530

    Fixes

--- brunch.sh
+++ brunch.sh
@@ -6,7 +6,7 @@ sudo apt-get --yes --force-yes install build-essential git git-svn bc binfmt-sup
 sudo apt-get --yes --force-yes install git-core gnupg flex bison gperf build-essential zip curl zlib1g-dev gcc-multilib g++-multilib libc6-dev-i386 lib32ncurses5-dev x11proto-core-dev libx11-dev lib32z-dev libgl1-mesa-dev libxml2-utils xsltproc unzip
 sudo apt-get --yes --force-yes install subversion ninja-build
 
-sudo apt-get --yes autoremove cmake
+sudo apt-get --yes remove cmake
 
 wget https://cmake.org/files/v3.12/cmake-3.12.2-Linux-x86_64.tar.gz
 tar -xzvf cmake-3.12.2-Linux-x86_64.tar.gz
